90aad32ba8ba3e548a3e253e32971bf9bab30c19
Mertane <ignacy.hebzda@gmail.com>
2023-06-28 18:15:00 +0200

    Added registering to backend and improved file structure

---
+++ server/pytter/models.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from pytter import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    profile_picture = db.Column(db.String(40), nullable=False, default='default.jpg')
+    password_hash = db.Column(db.String(128), nullable=False)
+    tweets = db.relationship('Tweet', backref='author', lazy=True)
+    likes = db.relationship('Like', backref='user', lazy=True)
+    account_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.profile_picture}', '{self.account_created}')"
+    
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    likes = db.relationship('Like', backref='tweet', lazy=True)
+
+    def __repr__(self):
+        return f"Tweet('{self.content}', '{self.date_posted}')"
+    
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    tweet_id = db.Column(db.Integer, db.ForeignKey('tweet.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Like('{self.user_id}', '{self.tweet_id}')"
+    
---
+++ server/pytter/routes.py
@@ -0,0 +1,34 @@
+from pytter import app, db, bcrypt
+from flask import request
+from flask_cors import cross_origin
+from models import User, Tweet, Like
+import re
+
+
+EMAIL_REGEX = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+@app.route("/whoami")
+@cross_origin(origin='http://localhost:3000', headers=['Authorization'])
+def whoami():
+    return {
+        "name": "John Doe",
+        "email": "john.doe@email.com",
+        "age": 42
+    }
+
+
+@app.route("/register", methods=["POST"])
+@cross_origin(origin='http://localhost:3000', headers=['Authorization'])
+def register():
+    data = dict(request.json)
+    if len(data["username"]) < 3 or len(data["username"]) > 20:
+        return {"message": "Username must be between 3 and 20 characters long"}, 400
+    if len(data["password"]) < 5 or len(data["password"]) > 20:
+        return {"message": "Password must be between 5 and 20 characters long"}, 400
+    if not re.match(EMAIL_REGEX, data["email"]):
+        return {"message": "Invalid email"}, 400
+    
+    hashed_password = bcrypt.generate_password_hash(data["password"]).decode("utf-8")
+    user = User(username=data["username"], email=data["email"], password_hash=hashed_password)
+    db.session.add(user)
+    db.session.commit()
