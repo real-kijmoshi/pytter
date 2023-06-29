d36a66a6bb3c927cb4ff8d4dc8268e0b94fc354c
Mertane <ignacy.hebzda@gmail.com>
2023-06-29 21:04:52 +0200

    added cors

--- server/pytter/__init__.py
+++ server/pytter/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pytter.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+CORS(app)
