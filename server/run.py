f47eb9a0bd9acd4c53c0fc707189185fe77e3587
kijmoshi <94071547+real-kijmoshi@users.noreply.github.com>
2023-07-01 16:25:22 +0200

    chyba nic

--- server/run.py
+++ server/run.py
@@ -1,6 +1,10 @@
-from pytter import app
+from pytter import app, db
 import pytter.routes
+from pytter.models import User, Tweet, Like
 
+
+app.app_context().push()
+db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
